d734f311d4a970c71c3ae5a22af3243cd88f7a63
Tory Stodard <tstodard26@gmail.com>
2022-03-22 16:27:20 -0600

    game playable in console

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-let cpuChoice = ["Rock","Paper","Scissors"];
+let cpuChoice = ["rock", "paper", "scissors"];
 let playerChoice;
 
-function cpuPlay(cpuChoice){
-    return cpuChoice[Math.floor(Math.random()*cpuChoice.length)];
+game();
+
+function game() {
+  for (i = 0; i < 5; i++) {
+    playRound();
+  }
 }
 
-function playerInput(playerChoice){
-   return playerChoice = prompt("Rock, Paper, or Scissors?");
+function playRound() {
+  cpuChoice = cpuInput();
+  playerChoice = playerInput();
+  playerChoice = playerChoice.toLowerCase();
+
+  if (cpuChoice == playerChoice) {
+    console.log("it's a tie!");
+  } else if (
+    (cpuChoice == "rock" && playerChoice == "paper") ||
+    (cpuChoice == "paper" && playerChoice == "scissors") ||
+    (cpuChoice == "scissors" && playerChoice == "rock")
+  ) {
+    console.log("you win this round!");
+  } else {
+    console.log("you loose this round!");
+  }
 }
 
-cpuPlay(cpuChoice);
-console.log(playerInput(playerChoice));
+function cpuInput() {
+  return cpuChoice[Math.floor(Math.random() * cpuChoice.length)];
+}
 
-
-
+function playerInput() {
+  return prompt("rock, paper, or scissors?");
+}
